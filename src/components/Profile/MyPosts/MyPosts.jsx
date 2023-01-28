7827eafd8f826f80fe5775d5e0e7b6475ff0cb99
Gregory <not.lost.in.code@gmail.com>
2023-01-29 00:00:30 +0400

    15 lesson

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import s from '../MyPosts/MyPosts.module.css'
+import Post from './Post/Post';
+
+const MyPosts = () => {
+    return (
+        <div>
+        My posts
+        <div>
+            New post
+        </div>
+        <div className={s.posts}>
+          <Post />
+        </div>
+    </div>
+    )
+}
+
+export default MyPosts;
